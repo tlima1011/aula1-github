70e317b398c2d8819d104f77248680bcf8be4c09
Thiago <thiagolima1011@gmail.com>
2020-12-11 20:29:20 -0300

    Mensagem de boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Date d = new Date(); 
+		
 		System.out.println("Bom dia!!!");
+		System.out.println("Boa Tarde");
 
 	}
 
