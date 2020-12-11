bb71b240250c598aed734c974c20bfbbd47c0fb4
Thiago <thiagolima1011@gmail.com>
2020-12-11 20:36:45 -0300

    Mensagem com data e hora

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Program {
@@ -7,9 +8,18 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Date d = new Date(); 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+		SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm"); 
 		
 		System.out.println("Bom dia!!!");
 		System.out.println("Boa Tarde");
+		System.out.println("Boa Noite!!!");
+		System.out.println("----------------------------");
+		System.out.println("Data de Hoje: "+sdf.format(d));
+		System.out.println("Hota atual: " +sdf1.format(d));
+		System.out.println("----------------------------");
+		
+		
 
 	}
 
